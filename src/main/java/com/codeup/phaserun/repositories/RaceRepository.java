d59e3da8e13b203e0179bb15a8643fe75aafdeb1
Robert Arroyos <robertarroyosjr@gmail.com>
2023-04-10 16:19:35 -0500

    Added Race/User Controllers and Race/User Repositories

--- src/main/java/com/codeup/phaserun/repositories/RaceRepository.java
+++ src/main/java/com/codeup/phaserun/repositories/RaceRepository.java
@@ -1,6 +1,9 @@
 package com.codeup.phaserun.repositories;
 
+import com.codeup.phaserun.models.Race;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RaceRepository extends JpaRepository {
+public interface RaceRepository extends JpaRepository<Race, Long> {
+    Race findById(int id);
+    Race findByRace_name(String Name);
 }
--- src/main/java/com/codeup/phaserun/repositories/UserRepository.java
+++ src/main/java/com/codeup/phaserun/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.codeup.phaserun.repositories;
 
+import com.codeup.phaserun.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findById(int id);
 }
