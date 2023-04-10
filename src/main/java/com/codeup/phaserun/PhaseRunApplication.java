27b7d4fecfe289b10b0599cc7c36026778b2445e
Chase Medford <chasemedford1@gmail.com>
2023-04-10 11:53:53 -0500

    Commit unversioned.

---
+++ src/main/java/com/codeup/phaserun/PhaseRunApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.phaserun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaseRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaseRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/phaserun/PhaseRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.phaserun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaseRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
