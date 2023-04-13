9a19912b0dac46b3271716e2f0a416f570850062
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-04-13 15:54:26 -0500

    working on raceSearch with post API logic

--- src/main/java/com/codeup/phaserun/controllers/RaceSearchController.java
+++ src/main/java/com/codeup/phaserun/controllers/RaceSearchController.java
@@ -1,18 +1,14 @@
 package com.codeup.phaserun.controllers;
 
-import com.codeup.phaserun.models.Race;
-import com.codeup.phaserun.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RaceSearchController {
 
-    @GetMapping("/racesearch")
+    @GetMapping("/raceSearch")
 
-    public String returnRaceSearchPage(Model model) {
-            model.addAttribute("races", new Race());
+    public String returnRaceSearchPage() {
         return "users/raceSearch";
     }
 
