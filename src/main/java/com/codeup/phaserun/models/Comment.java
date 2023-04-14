bbb332207257b4dcf522580df06f681f52047afa
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-04-14 14:38:46 -0500

    created comment model and removed excess from race model

---
+++ src/main/java/com/codeup/phaserun/models/Comment.java
@@ -0,0 +1,61 @@
+package com.codeup.phaserun.models;
+
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="comments")
+
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String comment;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Race race;
+
+
+    //contractors
+    public Comment() {
+    }
+
+    public Comment(String comment) {
+        this.comment = comment;
+    }
+
+    public Comment(int id, String comment) {
+        this.id = id;
+        this.comment = comment;
+    }
+
+    public Comment(int id, String comment, User user) {
+        this.id = id;
+        this.comment = comment;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
