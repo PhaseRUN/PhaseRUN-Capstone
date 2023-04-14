7f4d23bfc1223f40e0d4d75284e62394cba03341
Chase Medford <chasemedford1@gmail.com>
2023-04-14 14:08:21 -0500

    Navbar changes, footer adjustment, css changes. Too many changes to list.

--- src/main/java/com/codeup/phaserun/controllers/ProfileController.java
+++ src/main/java/com/codeup/phaserun/controllers/ProfileController.java
@@ -2,14 +2,13 @@ package com.codeup.phaserun.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProfileController {
 
     @GetMapping("/profile")
     public String returnControllerPage() {
-        return "users/profile";
+        return "/main/resources/static/deprecated/profile.html";
     }
 
 
