10526f2c189cae426cdb39b3f85cc6e493b200e5
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-04-17 13:54:55 -0500

    working on date comparison

--- src/main/java/com/codeup/phaserun/models/RaceInfo.java
+++ src/main/java/com/codeup/phaserun/models/RaceInfo.java
@@ -1,5 +1,7 @@
 package com.codeup.phaserun.models;
 
+import java.util.Date;
+
 public class RaceInfo {
 
     private String name;
@@ -14,6 +16,8 @@ public class RaceInfo {
 
     private String raceURL;
 
+    private Date startDate;
+
     public RaceInfo() {
     }
 
@@ -24,6 +28,16 @@ public class RaceInfo {
         this.logoUrl = logoUrl;
         this.raceId = raceId;
         this.raceURL = raceURL;
+    }
+
+    public RaceInfo(String name, String description, String date, String logoUrl, int raceId, String raceURL, Date startDate) {
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.logoUrl = logoUrl;
+        this.raceId = raceId;
+        this.raceURL = raceURL;
+        this.startDate = startDate;
     }
 
     public String getName() {
@@ -72,6 +86,14 @@ public class RaceInfo {
 
     public void setRaceURL(String raceURL) {
         this.raceURL = raceURL;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
     }
 }
 
