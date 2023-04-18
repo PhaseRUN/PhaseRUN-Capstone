b5b3cc70a34be5daf429f548b3d3a9b49158853a
Robert Arroyos <robertarroyosjr@gmail.com>
2023-04-18 12:08:11 -0500

    fitness edit updates activity level and running experience in Db

--- src/main/java/com/codeup/phaserun/controllers/ProfileController.java
+++ src/main/java/com/codeup/phaserun/controllers/ProfileController.java
@@ -4,10 +4,7 @@ import com.codeup.phaserun.models.User;
 import com.codeup.phaserun.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProfileController {
@@ -45,16 +42,7 @@ public class ProfileController {
         return "users/profile";
     }
 
-//    @PostMapping("/profile/{id}/edit")
-//    public String updateUser(@ModelAttribute User userUpdates, @PathVariable int id, Model model) {
-//        System.out.println(userUpdates);
-//        User userToUpdate = userDao.findById(userUpdates.getId());
-//        userToUpdate.setZipcode(userUpdates.getZipcode());
-//        System.out.println(userToUpdate);
-//        userDao.save(userToUpdate);
-//        User userFromDb = userDao.findById(id);
-//        model.addAttribute("user", userFromDb);
-//        return "users/profile";
-//    }
+//    @DeleteMapping
+//    public String deleteUser()
 
 }
