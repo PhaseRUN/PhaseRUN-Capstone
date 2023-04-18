a6c21341c2ec7b67d988bb0defa363bdac373c07
Chase Medford <chasemedford1@gmail.com>
2023-04-18 11:01:48 -0500

    Race cards created on profile. No functionality yet. Background added. Footer adjusted. Small changes everywhere.

--- src/main/java/com/codeup/phaserun/controllers/RaceSearchController.java
+++ src/main/java/com/codeup/phaserun/controllers/RaceSearchController.java
@@ -18,6 +18,8 @@ import java.util.List;
 @Controller
 public class RaceSearchController {
 
+
+
     @GetMapping("/race/search")
     public String returnRaceSearchPage() {
         return "users/raceSearch";
