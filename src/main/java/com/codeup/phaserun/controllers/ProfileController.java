c4a095a1490f1afe367404651bb36ee8e567415b
Chase Medford <chasemedford1@gmail.com>
2023-04-26 09:34:53 -0500

    Comments and info scroll within the card.

--- src/main/java/com/codeup/phaserun/controllers/ProfileController.java
+++ src/main/java/com/codeup/phaserun/controllers/ProfileController.java
@@ -3,6 +3,7 @@ package com.codeup.phaserun.controllers;
 import com.codeup.phaserun.repositories.RaceRepository;
 import com.codeup.phaserun.repositories.UserRepository;
 
+import org.jsoup.Jsoup;
 import org.springframework.security.core.Authentication;
 import com.codeup.phaserun.models.*;
 import com.codeup.phaserun.repositories.CommentRepository;
@@ -11,8 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
