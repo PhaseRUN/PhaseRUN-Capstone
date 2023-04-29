5fdb4cf8469a155be95337e88f6bf12be14b6641
Chase Medford <chasemedford1@gmail.com>
2023-04-29 14:15:21 -0500

    Merged with main. Comments need more work. Delete stopped working again. Info has been changed to visit and sends user to event link.

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -10,21 +10,6 @@ $('.comment-btn').click(function() {
     $(this).parent().find('.comment-section').toggle();
 });
 
-$('.info-btn').click(function() {
-    const infoSection = $(this).parent().find('.info-section');
-    const isActive = $(this).hasClass('active');
-
-    infoSection.toggle();
-    $(this).toggleClass('active');
-
-    if (isActive) {
-        infoSection.removeClass('text-info');
-    } else {
-        infoSection.addClass('text-info');
-    }
-});
-
-
 function hasClass(elem, className) {
     return elem.className.split(' ').indexOf(className) > -1;
 }
